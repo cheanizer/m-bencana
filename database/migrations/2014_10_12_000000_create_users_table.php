449b1205298265ce599d057476ece2de495c0612
Adhi Setyawan <stw.adhi@gmail.com>
2020-04-21 01:51:12 +0700

    update username length

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->nullable();
-            $table->string('username')->unique();
+            $table->string('username',50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('role_id')->nullable();
